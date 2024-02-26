fe39afd6be8bfa551e900af4fd01b61f0738c70e
nickl103 <nickl103@umn.edu>
2024-02-25 22:02:00 -0600

    data import complete

--- week6.R
+++ week6.R
@@ -1,7 +1,12 @@
 #Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+library(stringi)
 
 
 
-
+#Data Import
+citations <- stri_read_lines("../data/citations.txt", encoding= 'Windows-1252')
+citations_txt <- stri_remove_empty(citations)
+length(citations) - length(citations_txt)
+mean(str_length(citations_txt))
