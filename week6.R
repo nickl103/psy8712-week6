d37ae46039ed942caefe020e6ba83567f95486bb
nickl103 <nickl103@umn.edu>
2024-02-25 22:30:49 -0600

    began data cleaning. created tibble, removed quotes, extracted year, trying to figure out how to extract the first page number

--- week6.R
+++ week6.R
@@ -2,7 +2,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(stringi)
-
+library(rebus)
 
 
 #Data Import
@@ -12,7 +12,12 @@ length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
 
 #Data Cleaning
+####come back to it later
+
+citations_tbl <- tibble(line=1:length(citations_txt), cite= citations_txt) %>%
+  mutate(cite=str_replace_all(cite, pattern="[\"\']", replacement="")) %>%
+  mutate(year=str_extract(cite,pattern= one_or_more(DGT))) %>% #this works because it pulls the first number but it won't work with the next one because there are other numbers before it 
+  mutate(page_start=str_extract(cite, pattern= one_or_more(DGT))) ###????? come back to it later
 
 
-citations_tbl <- tibble(line=1:length(citations_txt), cite= citations_txt) 
   
