a0b0fd3c81fb2943655682ac2dd233541d429110
nickl103 <nickl103@umn.edu>
2024-02-25 22:09:36 -0600

    data cleaning started: tibble created.

--- week6.R
+++ week6.R
@@ -10,3 +10,9 @@ citations <- stri_read_lines("../data/citations.txt", encoding= 'Windows-1252')
 citations_txt <- stri_remove_empty(citations)
 length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
+
+#Data Cleaning
+
+
+citations_tbl <- tibble(line=1:length(citations_txt), cite= citations_txt) 
+  
