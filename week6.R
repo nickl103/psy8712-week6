9dd4943febd6e6fc41069967bdf77abd6a3a6d16
nickl103 <nickl103@umn.edu>
2024-02-26 12:24:50 -0600

    project 6 finished. Could not figure out how to get title to work correctly either with regex or rebus. I did the project mostly using rebus because it makes more sense to me that way even if it is more work to write out.

--- week6.R
+++ week6.R
@@ -12,12 +12,17 @@ length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
 
 #Data Cleaning
-####come back to it later
+View(sample_n(citations_tbl, 20))
 
 citations_tbl <- tibble(line=1:length(citations_txt), cite= citations_txt) %>%
   mutate(cite=str_replace_all(cite, pattern="[\"\']", replacement="")) %>%
-  mutate(year=str_extract(cite,pattern= one_or_more(DGT))) %>% #this works because it pulls the first number but it won't work with the next one because there are other numbers before it 
-  mutate(page_start=str_extract(cite, pattern= one_or_more(DGT))) ###????? come back to it later
+  mutate(year=str_extract(cite,pattern= one_or_more(DGT))) %>% 
+  mutate(page_start = str_extract(cite, pattern= capture(one_or_more(DGT)) %R% one_or_more(optional("-"))) ) %>%
+  mutate(perf_ref=str_detect(cite, pattern=regex("performance", ignore_case=T))) %>%
+  mutate(title=str_extract(cite, pattern = "(?<=\\))\\.(?=\\s)([^\\.]+[.?!])")) %>% 
+  mutate(first_author= str_extract(cite, pattern= START %R% one_or_more(WRD) %R% zero_or_more("-" %R% one_or_more(WRD)) %R% optional(",") %R% SPC %R% char_class("A-Z") %R% optional(SPC) %R% optional(DOT) %R% optional(SPC) %R% optional(char_class("A-Z"))))  
 
+sum(!is.na(citations_tbl$first_author))
+ 
 
   
